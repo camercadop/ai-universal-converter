6471f4dfd20dbef959d5137c8068f9576a32049a
Carlos Mercado <carlosmercadop714@gmail.com>
2026-07-14 09:34:57 -0500

    chore: update ESLint configuration and add lint script to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
     node: true,
     es2021: true,
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   rules: {},
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
 };
